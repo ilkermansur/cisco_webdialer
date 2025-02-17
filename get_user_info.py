806b520d3f033b6c2d6b17b3996722fb2963a06a
ilkermansur <59640302+ilkermansur@users.noreply.github.com>
2025-02-17 22:56:23 +0300

    Update get_user_info.py
    typo

--- get_user_info.py
+++ get_user_info.py
@@ -12,10 +12,10 @@ from requests.auth import HTTPBasicAuth
 disable_warnings(InsecureRequestWarning)
 
 ##################################################################################################################
-# Credential for CUCM AXL Application user
 username = 'axluser'
 password = 'Aa123456'
 fqdn = '192.168.85.80'
+userid = 'user01'
 ##################################################################################################################
 address = 'https://{}:8443/axl/'.format(fqdn)
 wsdl = 'AXLAPI.wsdl'
@@ -34,19 +34,22 @@ def show_history():
         print(etree.tostring(item["envelope"], encoding="unicode", pretty_print=True))  
 
 
-def get_user_info (userid):
+def get_user_info (userid=userid):
     """
     The function `get_user_info` retrieves user information such as user ID, telephone number, and
-    associated devices using the AXL API in Python.
+    associated devices using the `axl.getUser` method.
     
-    :param userid: The `get_user_info` function you provided seems to be using the `axl` module to
-    retrieve user information based on the `userid` parameter passed to the function. It then extracts
-    specific details such as `userid`, `telephoneNumber`, and `associatedDevices` from the response and
-    prints them
+    :param userid: The code snippet you provided seems to be a function that retrieves user information
+    using a library or API called `axl`. It takes a `userid` as input and returns the user's ID,
+    telephone number, and associated devices
+    :return: The `get_user_info` function is returning the `userid`, `telephone_number`, and
+    `controlled_devices` associated with the user specified by the input `userid`. The function makes a
+    call to `axl.getUser` to retrieve this information and then extracts and returns these specific
+    details.
     """
 
     try:
-        get_user_info = axl.getUser(userid='user01', returnedTags={
+        get_user_info = axl.getUser(userid=userid, returnedTags={
                                                             'userid':'',
                                                             'telephoneNumber':'',
                                                             'associatedDevices':''
@@ -56,21 +59,14 @@ def get_user_info (userid):
         telephone_number = get_user_info['return']['user']['telephoneNumber']
         controlled_devices = get_user_info['return']['user']['associatedDevices']['device']
 
-
-        print (
-            f'userid :{userid}\n',
-            f'telephone_number :{telephone_number}\n',
-            f'controlled_devices :{controlled_devices}'
-            )
+        return userid, telephone_number, controlled_devices
 
     except Fault as f:
         print (f)
         print (show_history())
 
-get_user_info(userid='user01')
+userid, telephone_number,controlled_devices =get_user_info()
 
-
-
-
-    
-
+print (f'userid : {userid} \n\
+telephony number : {telephone_number} \n\
+controlled devices : {controlled_devices}')
